9a44e9213a761e031e81b8d7d83582bd5be804e3
Shajan Ulhaque <s_ulhaque@yahoo.co.uk>
2024-11-25 15:06:30 +0000

    task 3 complete

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Northcoders News API
 
-For instructions, please head over to [L2C NC News](https://l2c.northcoders.com/courses/be/nc-news).
-
-
-
---- 
-
 This portfolio project was created as part of a Digital Skills Bootcamp in Software Engineering provided by [Northcoders](https://northcoders.com/)
+
+We'll have two databases in this project: one for real-looking dev data, and another for simpler test data.
+
+You will need to create two .env files for this project: .env.test with a path of PGDATABASE=nc_news_test and a .env.development with a path of PGDATABASE=nc_news.
+
+You'll need to run npm install.
---
+++ error-handler.js
@@ -0,0 +1,25 @@
+function notFoundErrorHandler (req, res) {
+    res.status(404).send({ message: "Sorry Not Found" });
+  }
+
+  function psqlErrorHandler (err, req, res, next)  {
+    if (err.code === "22P02") {
+      res.status(400).send({ message: "Bad request" });
+    } else {
+      next(err);
+    }
+  };
+  
+ function customErrorsHandler (err, req, res, next) {
+    if (err.status && err.message) {
+      res.status(err.status).send({ message: err.message });
+    } else {
+      next(err);
+  };
+}
+  
+  function serverErrorHandler(err, req, res, next) {
+    res.status(500).send({ msg: "Internal Server Error" });
+  };
+
+  module.exports = {notFoundErrorHandler, psqlErrorHandler, customErrorsHandler, serverErrorHandler}

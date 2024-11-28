2411859bf007692d471aacea2bc980464684692d
Shajan Ulhaque <s_ulhaque@yahoo.co.uk>
2024-11-28 09:52:28 +0000

    task 9 completed

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,21 +1,15 @@
 const {
   selectCommentsByArticleId,
   addComment,
+  removeSelectedComment,
 } = require("../models/comments.model")
 
-const checkCommentsExists = require("../models/checkIfExists")
-const checkArticleExists = require("../models/checkIfExists")
+const checkExists = require("../models/checkIfExists")
 
 function getCommentsByArticleId(req, res, next) {
   const { article_id } = req.params
-  const promises = [selectCommentsByArticleId(article_id)]
-  if (article_id) {
-    promises.push(checkArticleExists(article_id))
-  }
-  Promise.all(promises)
-    .then(([comments]) => { 
-        console.log(comments);
-             
+  selectCommentsByArticleId(article_id)
+    .then((comments) => {
       res.status(200).send({ comments })
     })
     .catch((err) => {
@@ -23,18 +17,47 @@ function getCommentsByArticleId(req, res, next) {
     })
 }
 
-function postCommentOnArticle(req, res, next) { 
+function postCommentOnArticle(req, res, next) {
   const { article_id } = req.params
   const { username, body } = req.body
   addComment(article_id, username, body)
-  .then((comment) => {
+    .then((comment) => {
       res.status(201).send({ comment })
     })
     .catch((err) => {
       next(err)
     })
 }
+// function deleteComment(req, res, next) {
+//   const { comment_id } = req.params
+//   const promises = [removeSelectedComment(comment_id)]
+//   if (comment_id) {
+//     promises.push(checkExists("comments", "comment_id", comment_id))
+//   }
+//   Promise.all(promises)
+//     .then(() => {
+//       res.status(204).send()
+//     })
+//     .catch((err) => {
+//       next(err)
+//     })
+// }
 
+//will return later and try and fix not working properly only works occasionally unsure why
 
-module.exports = { getCommentsByArticleId, postCommentOnArticle }
+function deleteComment(req, res, next) {
+  const { comment_id } = req.params
+  const promises = [
+    checkExists("comments", "comment_id", comment_id),
+    removeSelectedComment(comment_id),
+  ]
+  Promise.all(promises)
+    .then(() => {
+      res.status(204).send()
+    })
+    .catch((err) => {
+      next(err)
+    })
+}
 
+module.exports = { getCommentsByArticleId, postCommentOnArticle, deleteComment }

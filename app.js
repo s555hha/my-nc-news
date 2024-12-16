9537a523f52d2689861af8c640104cfa8c9dfeae
Shajan Ulhaque <s_ulhaque@yahoo.co.uk>
2024-12-16 19:04:32 +0000

    a

--- app.js
+++ app.js
@@ -12,12 +12,7 @@ const {
 } = require("./error-handler")
 
 const app = express()
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
+app.use(cors());
 app.use(express.json())
 app.use('/api', apiRouter)
 app.use(psqlErrorHandler)
